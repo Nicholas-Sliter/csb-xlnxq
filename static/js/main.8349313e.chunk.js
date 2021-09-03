1ea3d5a1e9ee0dd834624fd504946c1df002a12a
Code Sandbox <hello@codesandbox.io>
2021-09-03 20:37:36 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.8349313e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkatelyn-website-react"]=this["webpackJsonpkatelyn-website-react"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),s=n(3),r=n.n(s),i=(n(8),n(0));function a(){return Object(i.jsxs)("div",{className:"App",children:[Object(i.jsx)("h1",{children:"Hello Katelyn!"}),Object(i.jsx)("h2",{children:"Just type something here to make it show up"})]})}var o=document.getElementById("root");r.a.render(Object(i.jsx)(c.StrictMode,{children:Object(i.jsx)(a,{})}),o)},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.8349313e.chunk.js.map
